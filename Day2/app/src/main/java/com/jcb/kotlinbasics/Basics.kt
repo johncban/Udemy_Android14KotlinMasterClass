765f2c08fa79aa4a82666fee6b8459b0706c2686
Juan Carlo A. Banayo <johncban@hotmail.com>
2024-01-28 04:04:44 -0500

    Complete Basics Quiz

--- Day2/app/src/main/java/com/jcb/kotlinbasics/Basics.kt
+++ Day2/app/src/main/java/com/jcb/kotlinbasics/Basics.kt
@@ -1,6 +1,33 @@
 package com.jcb.kotlinbasics
 
+
+/*
+https://developer.android.com/kotlin/learn#:~:text=Kotlin%20uses%20two%20different%20keywords,variable%20whose%20value%20can%20change.
+ */
+
 fun main(){
+    /***
+    val num1 = 1 // val for variable value that never changes - immutable
+    var num2 : Short = 2 // var for variable value that changes - mutable
+
+    println(num2)
+
+
+
     var hello = "Hello "
     println(hello + "World!")
+
+    num2 = 32345
+
+    println(num2)
+    ***/
+
+    var pi : Float = 3.14F
+
+    println(pi)
+
+    pi = 3.1415926535F
+
+    println(pi)
+
 }
---
+++ Day2/app/src/main/java/com/jcb/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,8 @@
+package com.jcb.kotlinbasics
+
+
+fun main() {
+    val circleRatio : Double = 3.1415926535897932 //Float = //3.1415926535F //3.14F
+
+    println(circleRatio)
+}
